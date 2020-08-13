9872a5423af9825a52d620021fec52863e72c885
Vaidas Kublinskas <vaidas@dragonofdeath.lt>
2020-08-13 11:46:29 +0300

    Initial
    With ideas from saulius/dotfiles thnukid/dotfiles

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Install Homebrew
+if test ! $(which brew); then
+   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "Installing system packages"
+brew bundle install --file=Brewfile
+
+brew cleanup
+
+echo "Setuping neovim"
+npm install -g neovim
+pip install pynvim
+pip3 install pynvim
+stow nvim
+
+echo "Stow other"
+stow zsh
+
+stow tmux
+
+echo "Installing rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
---
+++ zsh/.zsh/configs/fzf.zsh
@@ -0,0 +1,30 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+# fbr - checkout git branch
+fbr() {
+  local branches branch
+  branches=$(git branch -vv) &&
+  branch=$(echo "$branches" | fzf +m) &&
+  git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
+}
+
+# fbr - checkout git branch (including remote branches)
+fbrr() {
+  local branches branch
+  branches=$(git branch --all | grep -v HEAD) &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
