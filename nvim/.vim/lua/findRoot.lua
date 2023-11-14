db309d2d3cd1b3fde9909ac3862bfdb521df6930
Vaidas Kublinskas (dragonofdeath) <vaidask@wix.com>
2023-11-14 23:07:15 +0200

    fix root resolution for oil.vim buffers

---
+++ nvim/.vim/lua/findRoot.lua
@@ -0,0 +1,45 @@
+-- function removeOilPrefix(inputString)
+--     -- Remove 'oil://' from the start of the string if it exists
+--     local result = string.gsub(inputString, "^oil://", "")
+--     return result
+-- end
+
+function getCurrentPath() 
+    local filetype = vim.bo.filetype
+    if (filetype == "oil") then
+        return require("oil").get_current_dir()
+    end
+    return vim.fn.expand("%:p")
+end
+
+function forPackage()
+    local path = getCurrentPath()
+    return vim.fn.system("find-package-root " .. path)
+end
+
+function forMonorepo()
+    local path = getCurrentPath()
+    return vim.fn.system("find-monorepo-root " .. path)
+end
+
+-- takes a command with placeholders like 'edit ${PACKAGE_ROOT}/package.json'
+-- and returns a function that will execute the command with the path placeholders filled
+function execWithRoot(cmd)
+    return function()
+        local result = cmd
+        if (string.find(cmd, "%${PACKAGE_ROOT}") ~= nil) then
+            local root = forPackage()
+            result = string.gsub(result, "%${PACKAGE_ROOT}", root)
+        end
+        if (string.find(cmd, "%${MONOREPO_ROOT}") ~= nil) then
+            local root = forMonorepo()
+            result = string.gsub(result, "%${MONOREPO_ROOT}", root)
+        end
+        return vim.cmd(result)
+    end
+end
+
+
+return {
+    execWithRoot = execWithRoot,
+}
